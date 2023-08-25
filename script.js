d838b47ef92727009ab514aae1e9390beeae31eb
megotto95 <megotto95@gmail.com>
2023-08-24 23:21:15 -0400

    Add memory for storing input and calculations
    calculator now works and will perform multiple operations before equals

--- script.js
+++ script.js
@@ -16,12 +16,7 @@ function divide(num1, num2) {
     return num1/num2;
 }
 
-
-let input1;
-let input2;
-let operator;
-
-function operate(num1, num2, op) {
+/*function operate(num1, num2, op) {
     if (op==="+") {
         return add(num1, num2);
     } else if (op==="-") {
@@ -31,5 +26,88 @@ function operate(num1, num2, op) {
     } else if (op==="/") {
         return divide(num1, num2);
     }
+}*/
+
+function operate(num1, num2, operator) {
+    if (operator=='+') {
+        let solution = add (+num1, +num2)
+        n1 = solution
+    displayContent.textContent = solution
+    } else if (operator=="-") {
+        let solution = subtract (+num1, +num2)
+        n1 = solution
+    displayContent.textContent = solution
+    } else if (operator=="*") {
+        let solution = multiply(+num1, +num2)
+        n1 = solution
+    displayContent.textContent = solution
+    } else if (operator=="/") {
+        let solution = divide(+num1, +num2)
+        n1 = solution
+    displayContent.textContent = solution
+    }
+    
 }
 
+function updateDisplay1(input) {
+    displayContent.textContent += input
+} 
+
+function updateDisplay2 (input) {
+    if (didOp)
+    {displayContent.textContent=""
+    displayContent.textContent = input
+    didOp = false}
+    else {
+        displayContent.textContent += input
+    }
+    
+}
+
+function clear () {
+    displayContent.textContent = ""
+    num1 = ""
+    num2 = ""
+}
+
+let num1 = ""
+let num2 = ""
+let op
+let didOp
+let displayContent = document.querySelector('.display')
+console.log(displayContent)
+
+let numbers = document.querySelectorAll('.number')
+let operators = document.querySelectorAll('.operator')
+
+numbers.forEach((number) => number.addEventListener('click', () => {
+    if (num1 == "") {
+     updateDisplay1(number.textContent)
+     console.log(number.textContent)
+     console.log(numbers)
+    }
+    else {
+        updateDisplay2(number.textContent)
+    }
+}))
+operators.forEach((operator) => operator.addEventListener('click', () => {
+                                        
+                                        if (num1 !== ""){
+                                            num2 = +displayContent.textContent
+                                        operate(num1, num2, op)
+                                        console.log(num2)}
+                                        op = operator.textContent
+                                        num1 = +displayContent.textContent
+                                        didOp = true
+                                        console.log(num1)
+                                        
+                                        }))
+
+
+document.getElementById('equals').addEventListener('click', () => {
+    num2 = +displayContent.textContent
+    console.log(`num2 is ${num2}`)
+    console.log(`op is ${op}`)
+    operate(num1, num2, op)})
+
+document.getElementById('clear').addEventListener('click', clear)
